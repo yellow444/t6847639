0f7cc0439b8628406786ef64306dbfb4740a6f47
yellow444 <yellow444@gmail.com>
2020-01-08 00:58:51 +0300

    Добавьте файлы проекта.

---
+++ t6847639/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace t6847639
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.histogramBox1 = new Emgu.CV.UI.HistogramBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть файл";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(219, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Закрыть программу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.histogramBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 371;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.button2);
+            this.splitContainer2.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.imageBox1);
+            this.splitContainer2.Size = new System.Drawing.Size(371, 450);
+            this.splitContainer2.SplitterDistance = 90;
+            this.splitContainer2.TabIndex = 1;
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox1.Location = new System.Drawing.Point(0, 0);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(371, 356);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // histogramBox1
+            // 
+            this.histogramBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.histogramBox1.Location = new System.Drawing.Point(0, 0);
+            this.histogramBox1.Name = "histogramBox1";
+            this.histogramBox1.Size = new System.Drawing.Size(425, 450);
+            this.histogramBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "t6847639";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.HistogramBox histogramBox1;
+    }
+}
+
---
+++ t6847639/Form1.cs
@@ -0,0 +1,98 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace t6847639
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+            openFileDialog1.Filter = "ori files (*.ori)|*.ori|All files (*.*)|*.*";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Выбираем и читаем файл
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                byte[] bytesfile = ReadBinFile(openFileDialog1.FileName);
+                // Если не пустой читаем размеры изображения 1-4 и 5-8 байты
+                if (bytesfile != BitConverter.GetBytes(0))
+                {
+                    int width = BitConverter.ToInt32(bytesfile, 0);
+                    int height = BitConverter.ToInt32(bytesfile, 4);
+                    byte[] imageData = new byte[width * height * 2];
+                    Array.Copy(bytesfile, 16, imageData, 0, imageData.Length);
+                    // Создаем пустое изображение
+                    Image<Gray, UInt16> depthImage = new Image<Gray, UInt16>(width, height);
+                    // Загружаем массив данных из файла
+                    depthImage.Bytes = imageData;
+                    // Копируем в imageBox1
+                    imageBox1.Image = depthImage.Copy().Convert<Bgra, byte>();
+                    // Создаем гистограмму
+                    GrayHist(depthImage.Copy());
+                    // Сохраняем результаты
+                    depthImage.Convert<Bgra, byte>().Save(Path.ChangeExtension(openFileDialog1.FileName, ".bmp"));
+                    Bitmap histo = new Bitmap(width, height);
+                    histogramBox1.DrawToBitmap(histo, new Rectangle(0, 0, histo.Width, histo.Height));
+                    histo.Save(Path.ChangeExtension(openFileDialog1.FileName, "histo.bmp"));
+                }
+            }
+        }
+
+        private void GrayHist(Image<Gray, UInt16> image)
+        {
+            histogramBox1.ClearHistogram();
+            histogramBox1.GenerateHistograms(image, 256);
+            histogramBox1.Refresh();
+        }
+
+        private static byte[] ReadBinFile(string filename)
+        {
+            byte[] bytes;
+            try
+            {
+                using (FileStream fsSource = new FileStream(filename,
+                    FileMode.Open, FileAccess.Read))
+                {
+                    // Read the source file into a byte array.
+                    bytes = new byte[fsSource.Length];
+                    int numBytesToRead = (int)fsSource.Length;
+                    int numBytesRead = 0;
+                    while (numBytesToRead > 0)
+                    {
+                        // Read may return anything from 0 to numBytesToRead.
+                        int n = fsSource.Read(bytes, numBytesRead, numBytesToRead);
+
+                        // Break when the end of the file is reached.
+                        if (n == 0)
+                            break;
+
+                        numBytesRead += n;
+                        numBytesToRead -= n;
+                    }
+                    numBytesToRead = bytes.Length;
+                    return bytes;
+                }
+            }
+            catch (FileNotFoundException Ex)
+            {
+                Console.WriteLine(Ex.Message);
+            }
+
+            return BitConverter.GetBytes(0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
